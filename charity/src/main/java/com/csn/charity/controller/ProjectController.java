6d1bcdc72376e7c35df52b90bbe7ad7ac35ba774
lephilong190702 <lephilong1907@gmail.com>
2023-09-16 15:11:41 +0700

    Long-update database

---
+++ charity/src/main/java/com/csn/charity/controller/ProjectController.java
@@ -0,0 +1,8 @@
+package com.csn.charity.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProjectController {
+    
+}
--- charity/src/main/java/com/csn/charity/service/ProjectService.java
+++ charity/src/main/java/com/csn/charity/service/ProjectService.java
@@ -1,11 +1,19 @@
 package com.csn.charity.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.csn.charity.repository.ProjectRepository;
 
 
 @Service
 public class ProjectService {
+    private final ProjectRepository projectRepository;
 
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
 
-
+    
 }

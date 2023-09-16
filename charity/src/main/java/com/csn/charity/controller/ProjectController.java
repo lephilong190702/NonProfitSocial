525acb80289356b74039916550e1521b1d7f9b0f
lephilong190702 <lephilong1907@gmail.com>
2023-09-16 20:54:04 +0700

    Long-update

--- charity/src/main/java/com/csn/charity/controller/ProjectController.java
+++ charity/src/main/java/com/csn/charity/controller/ProjectController.java
@@ -1,8 +1,22 @@
 package com.csn.charity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.csn.charity.service.interfaces.ProjectService;
+
+
 
 @Controller
 public class ProjectController {
-    
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("projects", projectService.getAllProjects());
+        return "index";
+    }
 }
---
+++ charity/src/main/java/com/csn/charity/service/interfaces/ProjectService.java
@@ -0,0 +1,9 @@
+package com.csn.charity.service.interfaces;
+
+import java.util.List;
+
+import com.csn.charity.model.Project;
+
+public interface ProjectService {
+    List<Project> getAllProjects();
+}

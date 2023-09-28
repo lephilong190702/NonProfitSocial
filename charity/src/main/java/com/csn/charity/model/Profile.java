24a97c3bb6fdf1fed3ef6a869431363c61c2b43d
lephilong190702 <lephilong1907@gmail.com>
2023-09-28 21:31:09 +0700

    [Long] - update

--- charity/src/main/java/com/csn/charity/model/Profile.java
+++ charity/src/main/java/com/csn/charity/model/Profile.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -48,5 +50,6 @@ public class Profile implements Serializable {
     @OneToOne
     @MapsId
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 }

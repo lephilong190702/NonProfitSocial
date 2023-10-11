76fd445e5e97db53b86f1b5014fcfad4eda14b16
lephilong190702 <lephilong1907@gmail.com>
2023-10-11 23:22:15 +0700

    [Long]-Xong chat

--- client/src/components/social/Chat/Chatbox.jsx
+++ client/src/components/social/Chat/Chatbox.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { FormControl, Button, InputGroup, Card } from 'react-bootstrap';
 import { UserContext } from '../../../App';
 import { authApi, endpoints } from '../../../configs/ApiConfig';
-import { addDoc, collection, getDocs, onSnapshot, orderBy, query, serverTimestamp, where } from 'firebase/firestore';
+import { addDoc, collection, doc, getDoc, getDocs, onSnapshot, orderBy, query, serverTimestamp, where } from 'firebase/firestore';
 import { db } from '../../../configs/Firebase';
 import { Link, useParams } from 'react-router-dom';
 import './Chatbox.css'; // Import file CSS
@@ -12,7 +12,9 @@ const Chatbox = () => {
   const [newMessage, setNewMessage] = useState("");
   const messageRef = collection(db, "messages");
   const userRef = collection(db, "users");
+  const [targetUser, setTargetUser] = useState(null)
   const [user] = useContext(UserContext);
+  const [senderData, setSenderData] = useState({});
 
   const sendMessage = async (evt) => {
     evt.preventDefault();
@@ -21,9 +23,8 @@ const Chatbox = () => {
     }
 
     try {
-      let res = await authApi().post(endpoints["chat"], {
+      let res = await authApi().post(endpoints["chat"](user.id), {
         content: newMessage,
-        userId: user.id,
       });
       setNewMessage("");
     } catch (error) {
@@ -38,15 +39,34 @@ const Chatbox = () => {
         orderBy("createAt")
       );
 
-      const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
-        let messages = [];
-        snapshot.forEach((doc) => {
-          messages.push({ ...doc.data(), id: doc.id });
+      const unsubscribe = onSnapshot(queryMessages, async (snapshot) => {
+        const promises = snapshot.docs.map(async (doc) => {
+          const messageData = doc.data();
+          const userRef = messageData.user;
+          const userDoc = await getDoc(userRef);
+          const userData = userDoc.data();
+          const displayName = userData.displayName;
+          const photoUrl = userData.photoUrl; // Lấy photoUrl từ dữ liệu người gửi
+          const userId = userData.id;
+          return { ...messageData, id: doc.id, userId, displayName, photoUrl };
         });
+      
+        const messages = await Promise.all(promises);
+        const senderData = {}; // Tạo đối tượng senderData mới
+      
+        // Lặp qua messages để cập nhật senderData
+        messages.forEach((message) => {
+          senderData[message.id] = { userId: message.userId, displayName: message.displayName, photoUrl: message.photoUrl };
+        });
+      
+        // Cập nhật trạng thái messages và senderData
         setMessages(messages);
+        setSenderData(senderData);
       });
-      return () => unsuscribe();
-    };
+      
+      return () => unsubscribe();
+      };
+
     fetchMessage();
   }, []);
 
@@ -58,13 +78,13 @@ const Chatbox = () => {
             <Card.Title>NONPROFIT SOCIAL NETWORK CHATGROUP</Card.Title>
             <hr />
             <div className="message-box">
-              {messages?.map((newMessage, i) => (
-                <div key={i} className={`message ${newMessage.userId === (user ? user.id : null) ? "message-right" : "message-left"}`}>
-                  {newMessage.userId && newMessage.userId !== (user ? user.id : null) && (
-                    <img src="/avatar.png" alt="Avatar" className="avatar" />
+              {messages?.map((message, i) => (
+                <div key={i} className={`message ${senderData[message.id]?.userId === (user ? user.id : null) ? 'message-right' : 'message-left'}`}>
+                  {/* Check if the message is not sent by the current user */}
+                  {senderData[message.id]?.userId !== (user ? user.id : null) && (
+                    <img src={senderData[message.id]?.photoUrl || "/avatar.png"} alt="Avatar" className="avatar" />
                   )}
-                  {newMessage.userId} -
-                  {newMessage.content}
+                  <div className="message-content">{message.content}</div>
                 </div>
               ))}
             </div>
--- client/src/configs/ApiConfig.js
+++ client/src/configs/ApiConfig.js
@@ -48,7 +48,7 @@ export const endpoints = {
     "vn-pay": (projectId) => `${SERVER_CONTEXT}/api/projects/${projectId}/donate/`,
     "user": `${SERVER_CONTEXT}/api/users/`,
     "userId": (userId) => `${SERVER_CONTEXT}/api/users/${userId}`,
-    "chat": `${SERVER_CONTEXT}/api/chat/`,
+    "chat": (userId) => `${SERVER_CONTEXT}/api/chat/${userId}`,
     "replies-post": (parentId) => `${SERVER_CONTEXT}/api/post-comment/${parentId}/replies/`,
     "comment-post": (postId) => `${SERVER_CONTEXT}/api/post/${postId}/comments/`,
 }

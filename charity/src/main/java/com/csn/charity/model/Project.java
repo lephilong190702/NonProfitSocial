968665de4803be2157c7605d4dd0f51599ed99b7
DatltCS <datluongtan1410@gmail.com>
2023-10-20 00:19:50 +0700

    Dat - Project DecialMin

--- charity/src/main/java/com/csn/charity/model/Project.java
+++ charity/src/main/java/com/csn/charity/model/Project.java
@@ -54,7 +54,7 @@ public class Project implements Serializable{
     private String content;
 
     @Column(name = "contributed_amount")
-    @DecimalMin(value = "0.01", message = "Giá trị phải lớn hơn 0")
+    // @DecimalMin(value = "0.01", message = "Giá trị phải lớn hơn 0")
     private BigDecimal contributedAmount;
 
     @Column(name = "total_amount", nullable = false)

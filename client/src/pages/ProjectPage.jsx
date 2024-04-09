5e581f41da495aa77346a8ee2e105f3000ba3136
DatltCS <datluongtan1410@gmail.com>
2024-04-10 02:16:20 +0700

    Dat - reback UI

--- client/src/pages/ProjectPage.jsx
+++ client/src/pages/ProjectPage.jsx
@@ -16,9 +16,27 @@ const ProjectPage = () => {
   const [q] = useSearchParams();
   const [showModal, setShowModal] = useState(false);
   const [selectedProjectId, setSelectedProjectId] = useState(null);
-  const [selectedProjectTitle, setSelectedProjectTitle] = useState("");
+  const currentURL = window.location.href;
+  const urlParams = new URLSearchParams(currentURL);
+  const url = new URL(currentURL);
+  const vnpAmount = url.searchParams.get("vnp_Amount");
+  const vnpBankCode = urlParams.get("vnp_BankCode");
+  const vnpBankTranNo = urlParams.get("vnp_BankTranNo");
+  const vnpCardType = urlParams.get("vnp_CardType");
+  const vnpOrderInfo = urlParams.get("vnp_OrderInfo");
+  const vnpPayDate = urlParams.get("vnp_PayDate");
+  const vnpResponseCode = urlParams.get("vnp_ResponseCode");
+  const vnpTmnCode = urlParams.get("vnp_TmnCode");
+  const vnpTransactionNo = urlParams.get("vnp_TransactionNo");
+  const vnpTransactionStatus = urlParams.get("vnp_TransactionStatus");
+  const vnpTxnRef = urlParams.get("vnp_TxnRef");
+  const vnpSecureHash = urlParams.get("vnp_SecureHash");
+  const [selectedProjectTitle, setSelectedProjectTitle] = useState(""); // Thêm state mới
 
   useEffect(() => {
+    console.log("vnpAmount: ", vnpAmount);
+    const savedProjectId = localStorage.getItem("selectedProjectId");
+
     const loadProject = async () => {
       try {
         let e = endpoints["project"];
@@ -26,7 +44,9 @@ const ProjectPage = () => {
 
         if (cateId !== null) {
           e = `${e}pcategories/${cateId}`;
-        } else {
+        }
+
+        else {
           let kw = q.get("kw");
           if (kw !== null) e = `${e}?kw=${kw}`;
         }
@@ -40,21 +60,34 @@ const ProjectPage = () => {
 
     const payment = async () => {
       const form = new FormData();
-      // Append payment data to the form here
+      form.append("vnp_Amount", vnpAmount);
+      form.append("vnp_BankCode", vnpBankCode)
+      form.append("vnp_BankTranNo", vnpBankTranNo)
+      form.append("vnp_CardType", vnpCardType)
+      form.append("vnp_OrderInfo", vnpOrderInfo)
+      form.append("vnp_PayDate", vnpPayDate)
+      form.append("vnp_ResponseCode", vnpResponseCode)
+      form.append("vnp_SecureHash", vnpSecureHash)
+      form.append("vnp_TmnCode", vnpTmnCode)
+      form.append("vnp_TransactionNo", vnpTransactionNo)
+      form.append("vnp_TransactionStatus", vnpTransactionStatus)
+      form.append("vnp_TxnRef", vnpTxnRef)
 
-      let e = await authApi().post(endpoints["callback"](selectedProjectId), form, {
+      let e = await authApi().post(endpoints["callback"](savedProjectId), form, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-    };
 
-    if (q.has("vnp_PayDate")) {
+    }
+
+    if (urlParams.has("vnp_PayDate")) {
       payment();
     }
 
     loadProject();
   }, [q]);
+
 
   const handlePayment = async () => {
     if (selectedProjectId === null) {
@@ -63,10 +96,29 @@ const ProjectPage = () => {
     }
 
     try {
-      // Handle payment submission here
+      const formData = new FormData();
+      formData.append("projectId", selectedProjectId);
+      formData.append("donateAmount", pay.donateAmount);
+      formData.append("note", pay.note);
+      let res = await authApi().post(endpoints["vn-pay"](selectedProjectId), formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      const redirectUrl = res.data;
+      console.log(res.data);
+
+      if (redirectUrl) {
+        window.location.href = redirectUrl;
+      } else {
+        console.error("Không có đường dẫn trả về từ server.");
+      }
+
     } catch (error) {
       console.error(error);
     }
+
   };
 
   const openModal = (projectId, projectTitle) => {
@@ -74,6 +126,7 @@ const ProjectPage = () => {
     setSelectedProjectId(projectId);
     setSelectedProjectTitle(projectTitle);
     setShowModal(true);
+
   };
 
   const closeModal = () => {
@@ -91,40 +144,43 @@ const ProjectPage = () => {
       </>
     );
 
-  return (
-    <div className="projects-container">
-      <h1 className="page-title">DANH SÁCH DỰ ÁN</h1>
-      <Row>
-        {project.map((p) => {
-          let url = `/projects/${p.id}`;
-          const maxContentHeight = 100;
-          const content = p.content.length > maxContentHeight ? p.content.substring(0, maxContentHeight) + '...' : p.content;
-          const maxTitleHeight = 50;
-          const title = p.title.length > maxTitleHeight ? p.title.substring(0, maxTitleHeight) + '...' : p.title;
-          
-          return (
-            <Col xs={12} md={3} key={p.id}>
-              <Card className="card">
-                <Card.Img variant="top" src={p.images && p.images.length > 0 ? p.images[0].image : ""} className="card-img" />
-                <Card.Body>
-                  <Card.Title className="card-title">{title}</Card.Title>
-                  <Card.Text className="card-text">{content}</Card.Text>
-                </Card.Body>
-                <Card.Footer className=" flex  bg-[#fff] border-[1px] border-[#DFDFDF]">
-                  <Link to={url} className="custom-card-link ">
-                    Xem chi tiết
-                  </Link>
-                  <div>
+    return (
+      <div className="projects-container">
+        <h1 className="page-title">DANH SÁCH DỰ ÁN</h1>
+        <Row>
+          {project.map((p) => {
+            let url = `/projects/${p.id}`;
+            
+            const maxContentHeight = 100; 
+            const content = p.content.length > maxContentHeight ? p.content.substring(0, maxContentHeight) + '...' : p.content;
+
+            const maxTitleHeight = 50; 
+            const title = p.title.length > maxTitleHeight ? p.title.substring(0, maxTitleHeight) + '...' : p.title;
+            
+            return (
+              <Col xs={12} md={3} key={p.id}>
+                <Card className="card">
+                  <Card.Img variant="top" src={p.images && p.images.length > 0 ? p.images[0].image : ""} className="card-img" />
+                  <Card.Body>
+                    <Card.Title className="card-title">{title}</Card.Title>
+                    <Card.Text className="card-text">{content}</Card.Text> 
+                    <Card.Footer>Số tiền đã quyên góp: {p.contributedAmount}</Card.Footer>
+                    <Card.Footer>Số tiền cần quyên góp: {p.totalAmount}</Card.Footer>
+                    <ProgressBar now={(p.contributedAmount / p.totalAmount) * 100} />
+                    <hr/>
+    
+                    <Link to={url} className="card-link">
+                      Xem chi tiết
+                    </Link>
                     <Button
                       onClick={() => openModal(p.id, p.title)}
-                      className="card-link donate-link custom-card-link "
+                      className="card-link donate-link"
                       style={{ marginRight: "5px" }}
                       variant="primary"
                     >
                       Đóng góp
                     </Button>
-                  </div>
-                </Card.Footer>
+                </Card.Body>
               </Card>
             </Col>
           );
@@ -132,7 +188,7 @@ const ProjectPage = () => {
       </Row>
       <Modal show={showModal} onHide={closeModal}>
         <Modal.Header closeButton>
-          <Modal.Title>{selectedProjectTitle}</Modal.Title>
+          <Modal.Title>{selectedProjectTitle}</Modal.Title> {/* Hiển thị tiêu đề ở đầu trang modal */}
         </Modal.Header>
         <Modal.Body>
           <Form>

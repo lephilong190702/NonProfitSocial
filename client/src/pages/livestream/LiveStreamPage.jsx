a96cf6393c620f02ad6a29b302e6143119809518
Phi Long <lephilong1907@gmail.com>
2024-04-20 10:56:51 +0700

    Final

--- client/src/pages/livestream/LiveStreamPage.jsx
+++ client/src/pages/livestream/LiveStreamPage.jsx
@@ -4,10 +4,15 @@ import { authApi, endpoints } from "../../configs/ApiConfig";
 
 const LiveStreamPage = () => {
     const [roomName, setRoomName] = useState('')
+    const [roomCode, setRoomCode] = useState('')
     const navigate = useNavigate();
 
     const handleRoomNameChange = (event) => {
         setRoomName(event.target.value);
+      };
+
+    const handleRoomCodeChange = (event) => {
+        setRoomCode(event.target.value);
       };
 
     const handleFormSubmit = async (ev) => {
@@ -23,6 +28,19 @@ const LiveStreamPage = () => {
         }
     }
 
+    const handleJoinRoomSubmit = async (ev) => {
+        ev.preventDefault();
+        try {
+            let response = await authApi().post(endpoints["join-room"](roomCode));
+            console.log('Joined room:', response.data);
+            if (response.status === 200) {
+                navigate(`/livestream/${roomCode}`);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div className="home-page">
             <form onSubmit={handleFormSubmit} className="form">
@@ -31,6 +49,13 @@ const LiveStreamPage = () => {
                     <input value={roomName} onChange={handleRoomNameChange} type="text" required placeholder="Enter Room Name" />
                 </div>
                 <button type="submit">Create Room</button>
+            </form>
+            <form onSubmit={handleJoinRoomSubmit} className="form">
+                <div>
+                    <label>Enter Room Code</label>
+                    <input value={roomCode} onChange={handleRoomCodeChange} type="text" required placeholder="Enter Room Code" />
+                </div>
+                <button type="submit">Join Room</button>
             </form>
         </div>
     )

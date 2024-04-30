2eec94538be2634056f500ae810b3d4310266be6
Phi Long <lephilong1907@gmail.com>
2024-04-30 15:41:38 +0700

    Update

--- charity/src/main/java/com/csn/charity/configs/WebSocketConfig.java
+++ charity/src/main/java/com/csn/charity/configs/WebSocketConfig.java
@@ -2,14 +2,12 @@ package com.csn.charity.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@CrossOrigin
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -21,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://34.101.52.106").withSockJS();
     }
     
 }

5b14bda1a914d501f9ee4ebad8ebe1b64874fd16
Phi Long <lephilong1907@gmail.com>
2024-05-01 21:23:08 +0700

    Update

--- charity/src/main/java/com/csn/charity/pay/VNPayConfig.java
+++ charity/src/main/java/com/csn/charity/pay/VNPayConfig.java
@@ -15,7 +15,7 @@ import java.util.*;
 public class VNPayConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_RefundUrl = "https://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
-    public static String vnp_ReturnUrl = "http://localhost:5173/projects/%s";
+    public static String vnp_ReturnUrl = "http://34.101.249.81/projects/%s";
     public static String vnp_TmnCode = "YVHL32H2";
     public static String secretKey = "BQLYPNICZVLJBHWATWWDROJFSLMPBDRX";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";

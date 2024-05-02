ceaf8eea1663177f74e32e528e210eb6db54e264
Phi Long <lephilong1907@gmail.com>
2024-05-02 14:15:36 +0700

    Update

--- charity/src/main/java/com/csn/charity/pay/VNPayService.java
+++ charity/src/main/java/com/csn/charity/pay/VNPayService.java
@@ -32,7 +32,8 @@ public class VNPayService {
         vnp_Params.put("vnp_CurrCode", "VND");
 
         vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-        vnp_Params.put("vnp_OrderInfo", orderInfor + " - Project ID: " + projectId + " - Username: " + user.getUsername());
+        vnp_Params.put("vnp_OrderInfo",
+                orderInfor + " - Project ID: " + projectId + " - Username: " + user.getUsername());
         vnp_Params.put("vnp_OrderType", orderType);
 
         String locate = "vn";
@@ -43,13 +44,26 @@ public class VNPayService {
         vnp_Params.put("vnp_ReturnUrl", urlReturn);
         vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        // Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        // SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        // String vnp_CreateDate = formatter.format(cld.getTime());
+        // vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        // cld.add(Calendar.MINUTE, 15);
+        // String vnp_ExpireDate = formatter.format(cld.getTime());
+        // vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-
-        cld.add(Calendar.MINUTE, 15);
-        String vnp_ExpireDate = formatter.format(cld.getTime());
+        System.out.println("CREATE DATE: " + vnp_CreateDate);
+        // Calculate the expiration date 15 minutes from now
+        cld.add(Calendar.MINUTE, 30);
+        Date expireDate = cld.getTime();
+        String vnp_ExpireDate = formatter.format(expireDate);
+        System.out.println("EXPIRE DATE: " + vnp_ExpireDate);
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 
         List fieldNames = new ArrayList(vnp_Params.keySet());

abd34d13a68634236ba7083ff0356c86e6bb68d4
Phi Long <lephilong1907@gmail.com>
2024-05-10 22:42:53 +0700

    Update

--- charity/src/main/java/com/csn/charity/controller/AddressController.java
+++ charity/src/main/java/com/csn/charity/controller/AddressController.java
@@ -37,13 +37,13 @@ public class AddressController {
     @RequestMapping(value = "/admin/accept/{addressId}", method = { RequestMethod.GET, RequestMethod.POST })
     public String approveAddress(@PathVariable Long addressId) {
         addressService.acceptAddress(addressId);
-        return "redirect:/address"; 
+        return "redirect:/admin/address"; 
     }
 
     @RequestMapping(value = "/admin/deny/{addressId}", method = { RequestMethod.GET, RequestMethod.POST })
     public String denyAddress(@PathVariable Long addressId) {
         addressService.rejectAddress(addressId);
-        return "redirect:/address";
+        return "redirect:/admin/address";
     }
 
     @GetMapping("/admin/address")

c31545b5dad7e246d0bdca8695ee5e919c41e914
DatltCS <datluongtan1410@gmail.com>
2024-06-14 12:04:52 +0700

    Dat - setup

--- client/src/pages/statistic/Statistic.jsx
+++ client/src/pages/statistic/Statistic.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import ApiConfig, { endpoints } from "../../configs/ApiConfig";
+import { Link } from "react-router-dom";
+import { saveAs } from "file-saver";
 
 const Statistic = () => {
-  return (
-    <div>Statistic</div>
-  )
-}
+  const exportFinancialReport = async () => {
+    try {
+        let e = endpoints.statistic;
+        e = `${e}?period=yearly&year=2023`
+      const response = await ApiConfig.get(e, {
+        responseType: "arraybuffer",
+      });
 
-export default Statistic
+      const blob = new Blob([response.data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+      saveAs(blob, "financial_report.xlsx");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      <Link href="#financial_report" onClick={exportFinancialReport}>
+        BÁO CÁO TÀI CHÍNH
+      </Link>
+    </div>
+  );
+};
+
+export default Statistic;

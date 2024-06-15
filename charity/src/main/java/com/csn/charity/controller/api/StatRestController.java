6c1448c3c7dee23c12c67f2b7ce89e9fde3894d5
lephilong190702 <lephilong1907@gmail.com>
2024-06-16 02:22:03 +0700

    Long - Update stat

--- charity/src/main/java/com/csn/charity/controller/api/StatRestController.java
+++ charity/src/main/java/com/csn/charity/controller/api/StatRestController.java
@@ -2,12 +2,10 @@ package com.csn.charity.controller.api;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
+import java.sql.Date;
+import java.time.ZoneId;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.apache.poi.ss.usermodel.*;
 import com.csn.charity.model.UserContributeProject;
 import com.csn.charity.service.interfaces.DonateService;
 import com.csn.charity.service.interfaces.StatService;
@@ -155,21 +153,36 @@ public class StatRestController {
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet("Contributions");
 
+        CellStyle currencyStyle = workbook.createCellStyle();
+        DataFormat dataFormat = workbook.createDataFormat();
+        currencyStyle.setDataFormat(dataFormat.getFormat("#,##0"));
+
+        CellStyle dateStyle = workbook.createCellStyle();
+        CreationHelper createHelper = workbook.getCreationHelper();
+        dateStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd/MM/yyyy"));
+
         // Create header row
         Row headerRow = sheet.createRow(0);
         headerRow.createCell(0).setCellValue("DỰ ÁN");
         headerRow.createCell(1).setCellValue("TÀI KHOẢN QUYÊN GÓP");
         headerRow.createCell(2).setCellValue("SỐ TIỀN QUYÊN GÓP");
-        headerRow.createCell(3).setCellValue("NGÀY QUYÊN GÓP");
+        headerRow.createCell(3).setCellValue("HIỆN VẬT QUYÊN GÓP");
+        headerRow.createCell(4).setCellValue("NGÀY QUYÊN GÓP");
 
+        headerRow.getCell(2).setCellStyle(currencyStyle);
         // Add data rows
         int rowNum = 1;
         for (UserContributeProject contribution : contributions) {
             Row row = sheet.createRow(rowNum++);
             row.createCell(0).setCellValue(contribution.getProject().getTitle());
             row.createCell(1).setCellValue(contribution.getUser().getUsername());
-            row.createCell(2).setCellValue(contribution.getDonateAmount().doubleValue());
-            row.createCell(3).setCellValue(contribution.getDonateDate().toString());
+            Cell amountCell = row.createCell(2);
+            amountCell.setCellValue(contribution.getDonateAmount().doubleValue());
+            amountCell.setCellStyle(currencyStyle);
+            row.createCell(3).setCellValue(contribution.getDonateItem());
+            row.createCell(4).setCellValue(
+                    Date.from(contribution.getDonateDate().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+            row.getCell(4).setCellStyle(dateStyle);
         }
 
         // Write workbook to response
